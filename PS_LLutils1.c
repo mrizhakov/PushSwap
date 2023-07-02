ac7c1606241140504b0820791814657690e3bc49
Michael Rizhakov <m.rizhakov@gmail.com>
2023-07-02 14:33:02 +0200

    Normalize_indexes function works and assigns correct indexes, refactoring it to save space and get rid of array of sorted ints

--- PS_LLutils1.c
+++ PS_LLutils1.c
@@ -83,6 +83,74 @@ void bubble_sort(t_stack **head) {
 }
 //function to assign normalized indexes
 //currently only finds the min value of the list
+
+
+
+//my version which works sorts only negative numbers
+
+void normalize_indexes(t_stack **head)
+{
+	if (*head == NULL)
+		return; // List is empty or has only one node, no sorting needed
+	t_stack *temp = *head;
+	int *sorted_list = (int *)malloc(sizeof(int)* 100);
+	int y = 0;
+	int lst_size;
+	sorted_list[0] = INT_MAX;
+	lst_size = ft_lstsize(temp);
+	temp = *head;
+//	while (temp != NULL)
+//	{
+//		if (temp->content <= sorted_list[y])
+//		{
+//			sorted_list[y] = temp->content;
+//			temp->index = y;
+//		}
+//		temp = temp->next;
+//	}
+//	y++;
+	while(y != lst_size)
+	{
+		temp = *head;
+		sorted_list[y] = INT_MAX;
+		while (temp != NULL)
+		{
+			if (y == 0)
+			{
+				if (temp->content <= sorted_list[y])
+				{
+					sorted_list[y] = temp->content;
+					temp->index = y;
+				}
+			}
+			if ((temp->content <= sorted_list[y]) && (temp->content > sorted_list[y-1]) )
+			{
+				sorted_list[y] = temp->content;
+				temp->index = y;
+				//printf("Algo says content is %i index is %i\n",  temp->content, temp->index);
+			}
+			temp = temp->next;
+		}
+		y++;
+	}
+//	int x = 0;
+//	while (x != y)
+//	{
+//		printf("Minimum value is : %i index is %i\n", sorted_list[x], x);
+//		x++;
+//	}
+//	temp = *head;
+//	while (temp != NULL)
+//	{
+//		printf("Node values is: %d, normalized value (index) is %d\n", temp->content, temp->index);
+//		temp = temp->next;
+//	}
+	temp = *head;
+	*head = temp;
+}
+
+//old version of normalize indexes which kind of works for first element only maybe also for the second one
+/*
 void normalize_indexes(t_stack **head)
 {
 	if (*head == NULL) {
@@ -91,35 +159,50 @@ void normalize_indexes(t_stack **head)
 	t_stack *temp = *head;
 	int *min = (int *)malloc(sizeof(int)* 100);
 	int i;
-	int y = 0;
+	int y = 1;
 
 
-
-	i = 0;
+	i = 1;
+	min[0] = INT_MIN;
+	int lowest_previous;
+	lowest_previous = 0;
 	//min = &temp->content;
-	min[0] = temp->content;
+	//min[0] = temp->content;
 
 	//temp->index = i;
+	min[y] = temp->content;
 
 
-	while (temp->next != NULL)
+	while (y != 6)
 	{
-			if (temp->next->content < min[0])
+		temp = *head;
+		//min[y] = temp->content;
+
+		while (temp->next != NULL) {
+			if ((temp->next->content < min[y]) && (min[y] > min[y-1]) )
 			{
 				//temp->next->index = i;
-				min[y] = temp->next->content;
-				y++;
+
+					min[y] = temp->next->content;
+
+
 			}
-		temp = temp->next;
+			temp = temp->next;
+		}
+		y++;
 	}
 	printf("Minimum value is : %d\n", min[0]);
 	printf("Minimum value is : %d\n", min[1]);
-	printf("Minimum value is : %d\n", min[2]);
+	printf("Minimum value is : %d\n", min[3]);
+	printf("Minimum value is : %d\n", min[4]);
+	printf("Minimum value is : %d\n", min[5]);
+
+
 
 
 	printf("Index is : %d\n", i);
 }
-
+**/
 
 t_stack *swap_nodes_by_value(t_stack **head, int val1, int val2) {
 	if (head == NULL || val1 == val2) {
